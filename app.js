4a6a4bbbb3a5bd3323e8fff5bc552745b3bfad05
julbrandao <juliaduartebrandao@gmail.com>
2024-11-04 10:58:56 -0300

    Atualização 2.3

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
- 
+
+app.use(express.static(path.join(__dirname, 'public'))); 
 // Configuração do EJS como view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
